eea36cc6ec0ae778657e6ab3bb928e9aab5fecef
DaVinCi <fox.winner9@gmail.com>
2025-03-08 17:52:17 +0300

    remove test

--- src/test/java/ru/ns/t_jobs/TJobsApplicationTests.java
+++ src/test/java/ru/ns/t_jobs/TJobsApplicationTests.java
@@ -1,13 +1,8 @@
 package ru.ns.t_jobs;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TJobsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
